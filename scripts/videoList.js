60bafc11be2e0110b21ced8f0c2c83e7193516c7
Publikwerker <jason.michael.hoffman@gmail.com>
2018-12-28 11:43:50 -0800

    transferred handleFormSubmit and render functions over to videoList

--- scripts/videoList.js
+++ scripts/videoList.js
@@ -1,4 +1,12 @@
 const videoList = (function(){
+
+
+/**
+ * @function generateVideoItemHtml
+ * Template function, creates an HTML string from a single decorated video object
+ * @param   {object} video - decorated video object
+ * @returns {string} HTML 
+ */
     const generateListItem = function(video){
         return(`
         <li>
@@ -11,7 +19,36 @@ const videoList = (function(){
         </li> 
         `);
     };
-    return {
-        generateListItem
+
+/**
+ * @function render
+ * Responsible for scanning store and rendering the video list to DOM
+ */
+    const render = function() {
+        const videosToDisplay = store.videos.map(item => generateListItem(item));
+        $('.results').html(videosToDisplay);
     };
+
+    const handleFormSubmit = function() {
+        $('form').on('submit', function(event) {
+            event.preventDefault();
+            const queryTerm = $('#search-term').val();
+            $('#search-term').val("");
+            api.fetchVideos(queryTerm, (response => {
+                store.setVideos(response);
+                render();
+            }));
+        });
+    };
+
+    const bindEventListeners = function(){
+        handleFormSubmit();
+    };
+
+    return {
+        generateListItem,
+        bindEventListeners,
+        render
+    };
+
 }());
